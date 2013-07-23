8cd766987cc3dd9951b087d3885e7e3e4375c22b
Andrew Turley Local <aturls@aturley-mac.local>
2013-07-23 18:32:37 -0400

    Finish up dealer, move shuffle to deck module.

---
+++ 8/deck.erl
@@ -0,0 +1,9 @@
+-module(deck).
+-export([shuffle/1]).
+
+shuffle(List) -> shuffle(List, []).
+shuffle([], Acc) -> Acc;
+shuffle(List, Acc) ->
+    {Leading, [H | T]} = lists:split(random:uniform(length(List)) - 1, List),
+    shuffle(Leading ++ T, [H | Acc]).
+
