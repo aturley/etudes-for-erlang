5f78700eb2ffbc378a62be0ccf324ccfe2f8c97a
Andrew Turley Local <aturls@aturley-mac.local>
2013-10-02 16:37:28 -0400

    Add documentation.

--- 2/geom.erl
+++ 2/geom.erl
@@ -1,12 +1,20 @@
 %% @author Andrew Turley
+%% @doc Functions for calculating areas of geometric shapes.
+%% @copyright 2013 Andrew Turley
+%% @version 0.1
 
 -module(geom).
 
 -export([area/2]).
 
+%% @doc Find the area of a rectangle given the height and width.
+
+-spec(area(number(),number()) -> number()).
 
 area(Height, Width) ->
-    volume(Height, Width, 1).
+    Height * Width.
+
+
 
 
 
